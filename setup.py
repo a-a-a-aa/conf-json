e60f195c9ea197d33d845b0f067e307615524bde
a-a-a-aa <astefard@gmail.com>
2021-12-24 18:47:43 +0500

    init

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="conf-json",
+    name="conf.json",
     version="0.0.1",
     author="Ali kamran",
     author_email="astefard@gmail.com",
